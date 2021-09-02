5de63847c348352579e7f3ecb918bd200d4f0557
Peter Ďurica <pdurica@live.com>
2021-09-02 23:45:43 +0200

    kubíkovacie funkcie

---
+++ Kubikovanie/Gulatina.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Kubikovanie
+{
+    public static class Gulatina
+    {
+        public static double Objem(Drevina drevina, decimal dlzka, int priemer)
+        {
+            var d = Convert.ToDouble(dlzka);
+            double hrubkaKory = default;
+
+            switch (drevina)
+            {
+                case Drevina.BK:
+                    hrubkaKory = BK(priemer);
+                    break;
+                case Drevina.BO:
+                    hrubkaKory = BO(priemer);
+                    break;
+                case Drevina.BP:
+                    hrubkaKory = BP(priemer);
+                    break;
+                case Drevina.DB:
+                    hrubkaKory = DB(priemer);
+                    break;
+                case Drevina.JD:
+                    hrubkaKory = JD(priemer);
+                    break;
+                case Drevina.SC:
+                    hrubkaKory = SC(priemer);
+                    break;
+                case Drevina.SM:
+                    hrubkaKory = SM(priemer);
+                    break;
+                case Drevina.TK:
+                    hrubkaKory = TK(priemer);
+                    break;
+            }
+            return Math.PI * Math.Pow(priemer - hrubkaKory, 2) / 4 * d / 10000;
+        }
+
+        static double BK(int priemer)
+        {
+            return 1.19461964238995E-11 * Math.Pow(priemer, 6) - 5.05397678443814E-09 * Math.Pow(priemer, 5) + 7.79245900812964E-07 * Math.Pow(priemer, 4) - 0.0000513269850701548 * Math.Pow(priemer, 3) + 0.00118592490866831 * Math.Pow(priemer, 2) + 0.0239937532546719 * priemer + 0.210777395515834;
+        }
+
+        static double BO(int priemer)
+        {
+            return 9.27178571495223E-10 * Math.Pow(priemer, 6) - 1.50798471619529E-07 * Math.Pow(priemer, 5) + 8.32050198873446E-06 * Math.Pow(priemer, 4) - 0.000167028326067964 * Math.Pow(priemer, 3) + 0.00104262346735595 * Math.Pow(priemer, 2) + 0.0214817576174937 * priemer + 0.202494479817688;
+        }
+
+        static double BP(int priemer)
+        {
+            return 5.85036123041673E-11 * Math.Pow(priemer, 6) - 1.93011802183257E-08 * Math.Pow(priemer, 5) + 2.37255515328494E-06 * Math.Pow(priemer, 4) - 0.000130900925448429 * Math.Pow(priemer, 3) + 0.00318264190389073 * Math.Pow(priemer, 2) + 0.0422912207895934 * priemer + 0.902261672581517;
+        }
+
+        static double DB(int priemer)
+        {
+            return 4.18512704799441E-12 * Math.Pow(priemer, 6) - 1.33655103434424E-09 * Math.Pow(priemer, 5) + 1.0949526957349E-07 * Math.Pow(priemer, 4) + 2.57829850092151E-06 * Math.Pow(priemer, 3) - 0.000590819628200734 * Math.Pow(priemer, 2) + 0.07432465524289 * priemer + 1.0578431753147;
+        }
+
+        static double JD(int priemer)
+        {
+            return 1.58102440172983E-09 * Math.Pow(priemer, 4) - 1.83551588452124E-06 * Math.Pow(priemer, 3) + 0.000212910426383939 * Math.Pow(priemer, 2) + 0.0410112234102087 * priemer + 0.0517094290546538;
+        }
+
+        static double SC(int priemer)
+        {
+            return -2.05743772340513E-11 * Math.Pow(priemer, 6) + 7.03868552071746E-09 * Math.Pow(priemer, 5) - 9.37434211256694E-07 * Math.Pow(priemer, 4) + 0.0000595492971910971 * Math.Pow(priemer, 3) - 0.0013527786738905 * Math.Pow(priemer, 2) + 0.0985307898899919 * priemer + 0.379685329131462;
+        }
+        
+        static double SM(int priemer)
+        {
+            return 2.43636149503342E-11 * Math.Pow(priemer, 6) - 9.76333250205831E-09 * Math.Pow(priemer, 5) + 1.43178308180392E-06 * Math.Pow(priemer, 4) - 0.0000935604731431283 * Math.Pow(priemer, 3) + 0.00268965152582723 * Math.Pow(priemer, 2) + 0.000485249067423865 * priemer + 0.39242984365228;
+        }
+
+        static double TK(int priemer)
+        {
+            return 5.33393933244535E-11 * Math.Pow(priemer, 6) - 2.00793461467216E-08 * Math.Pow(priemer, 5) + 2.72667327691489E-06 * Math.Pow(priemer, 4) - 0.000157970776211158 * Math.Pow(priemer, 3) + 0.00363928456312124 * Math.Pow(priemer, 2) + 0.0327958959624084 * priemer + 0.446159898907354;
+        }         
+
+        public enum Drevina
+        {
+            BK,
+            BO,
+            BP,
+            DB,
+            JD,
+            SC,
+            SM,
+            TK
+        }
+    }
+}
---
+++ UnitTesty/Gulatina.cs
@@ -0,0 +1,136 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesty
+{
+    [TestClass]
+    public class Gulatina
+    {
+        [TestMethod]
+        public void TestBK_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BK, 4, 24, 0.166, 0.0005);
+        }
+        [TestMethod]
+        public void TestBK_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BK, 4, 25, 0.181, 0.0002);
+        }
+        [TestMethod]
+        public void TestBK_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BK, 4, 26, 0.196, 0.0002);
+        }
+        [TestMethod]
+        public void TestBO_14_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BO, 4, 14, 0.0573, 0.000001);
+        }
+        [TestMethod]
+        public void TestBO_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BO, 4, 25, 0.184, 0.00025);
+        }
+        [TestMethod]
+        public void TestBO_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BO, 4, 26, 0.2, 0.001);
+        }
+        [TestMethod]
+        public void TestBP_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BP, 4, 25, 0.158);
+        }
+        [TestMethod]
+        public void TestBP_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BP, 4, 24, 0.144);
+        }
+        [TestMethod]
+        public void TestBP_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.BP, 4, 26, 0.171);
+        }
+        [TestMethod]
+        public void TestDB_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.DB, 4, 24, 0.144, 0.001);
+        }
+        [TestMethod]
+        public void TestDB_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.DB, 4, 25, 0.158, 0.001);
+        }
+        [TestMethod]
+        public void TestDB_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.DB, 4, 26, 0.171, 0.001);
+        }
+        [TestMethod]
+        public void TestJD_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.JD, 4, 24, 0.165, 0.001);
+        }
+        [TestMethod]
+        public void TestJD_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.JD, 4, 25, 0.180, 0.002);
+        }
+        [TestMethod]
+        public void TestJD_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.JD, 4, 26, 0.195, 0.003);
+        }
+        [TestMethod]
+        public void TestSC_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.SC, 4, 24, 0.15, 0.006);
+        }
+        [TestMethod]
+        public void TestSC_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.SC, 4, 25, 0.16, 0.005);
+        }
+        [TestMethod]
+        public void TestSC_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.SC, 4, 26, 0.17, 0.003);
+        }
+        [TestMethod]
+        public void TestSM_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.SM, 4, 24, 0.165, 0.0004);
+        }
+        [TestMethod]
+        public void TestSM_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.SM, 4, 25, 0.180, 0.0006);
+        }
+        [TestMethod]
+        public void TestSM_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.SM, 4, 26, 0.195, 0.00061);
+        }
+        [TestMethod]
+        public void TestTK_24_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.TK, 4, 24, 0.15, 0.006);
+        }
+        [TestMethod]
+        public void TestTK_25_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.TK, 4, 25, 0.17, 0.005);
+        }
+        [TestMethod]
+        public void TestTK_26_4()
+        {
+            AssertDiff(Kubikovanie.Gulatina.Drevina.TK, 4, 26, 0.18, 0.003);
+        }
+
+        void AssertDiff(Kubikovanie.Gulatina.Drevina dr, decimal dlzka, int priemer, double expected, double maxDiff = 0.01)
+        {
+            var result = Kubikovanie.Gulatina.Objem(dr, dlzka, priemer);
+            var diff = System.Math.Abs(result - expected);
+            Assert.IsTrue(diff < maxDiff);
+        }
+    }
+}
